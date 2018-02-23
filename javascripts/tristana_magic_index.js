17b8a40fe5c5b4c5129061997fd5b8b1eb298e91
Eric Hsieh <eric.kf.hsieh@gmail.com>
2018-02-23 14:10:54 -0800

    trying out js

---
+++ javascripts/tristana_magic_index.js
@@ -0,0 +1,19 @@
+var d100_effects = {
+	'1':"yes",
+	'2':"no",
+	'3':"maybe",
+	'4':"poop",
+	'5':"sundae"
+}
+
+var randomProperty = function (obj) {
+    var keys = Object.keys(obj)
+    return obj[keys[ keys.length * Math.random() << 0]];
+};
+
+function get_d100_effects()
+{
+	var keys = Object.keys(d100_effects)
+	return d100_effects[keys[keys.length * Math.random() << 0]]
+	// << 0 basically does Math.floor()
+}
