b0fde5c927e694d506009093bd0380fec45eacaa
Eric Hsieh <eric.kf.hsieh@gmail.com>
2018-03-29 20:35:09 -0700

    Large changes to website style & layout. Official version is now bootstrap.

--- javascripts/dnd_tristana2.js
+++ javascripts/dnd_tristana2.js
@@ -5,3 +5,17 @@ $(function () {
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(window).on('load, resize', function navUpdate() {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 600) {
+        $(".view").removeClass("view view-portfolio").addClass("gallery-mobile");
+    }
+});
+
+function add_hp(n)
+{
+    var currHP = parseInt(document.getElementById("hp_display").innerHTML);
+    var newHP = currHP + n;
+    document.getElementById("hp_display").innerHTML = newHP;
+}
